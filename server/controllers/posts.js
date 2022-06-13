506f4dbba2cc51c183cb55f38c8ba5ecdc5b5dcb
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-13 10:58:56 +0100

    routes post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,15 @@
+import PostMessage from "../models/postMessage.js";
+
 export const getPosts = (req, res) => {
-    res.send('This Works');
+    try{
+       const postMessages = PostMessage.find() ;
+
+       console.log(postMessages)
+    } catch (error){
+        res.status(404).json({Error: error.message})
+    }
+    
+}
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 }
