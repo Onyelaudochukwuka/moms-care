88ea9976b8f2df1c05ca0598f7e20cd8be005db6
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-06-13 12:07:37 +0100

    setting up env

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 app.use('/posts', postRoutes);
@@ -11,13 +12,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-const CONNECTION_URL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.bmcxh.mongodb.net/?retryWrites=true&w=majority`;
-
+const CONNECTION_URL = `mongodb+srv://${process.env.USERNAME.toLowerCase()}:${process.env.PASSWORD}@cluster0.bmcxh.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log({ Error: error.message }));
-    
+
 // mongoose.set('useFindAndModify', false);
 // https://mongodb.com/cloud/atlas
